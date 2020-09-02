7b71df5ce6d7df097dc20cd603ff2fd004645751
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-09-02 14:16:01 +0900

    [edit]差分の変更

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
 import React from "react";
 import AppNavigater from "./navigation/AppNavigator";
-
-export default function App() {
-  return <AppNavigater />;
-}
-import React from "react";
-import AppNavigater from "./navigation/AppNavigator";
 import { Provider } from "react-redux";
 import store from "./store";
 
