b3e245a842b484566a8f7ebc1f11babda2cecb7a
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-09-02 12:53:15 +0900

    [edit]storeの実装

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import AppNavigater from "./navigation/AppNavigator";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default function App() {
-  return <AppNavigater />;
+  return (
+    <Provider sotre={store}>
+      <AppNavigater />;
+    </Provider>
+  );
 }
