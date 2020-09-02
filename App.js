097f90183490b6c0a8e09636f2ac37060523cae3
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2020-09-02 13:49:53 +0900

    [edit]Reduxまで戻した

--- App.js
+++ App.js
@@ -4,3 +4,15 @@ import AppNavigater from "./navigation/AppNavigator";
 export default function App() {
   return <AppNavigater />;
 }
+import React from "react";
+import AppNavigater from "./navigation/AppNavigator";
+import { Provider } from "react-redux";
+import store from "./store";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AppNavigater />;
+    </Provider>
+  );
+}
